590437abaf226be665cbd07dbe9e6f822040b5ff
Brian Coutts <bjhcoutts@gmail.com>
2023-10-19 16:04:16 -0700

    test background image

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,39 @@
-<h1>Scroll Down!</h1>
+<script>
+	import PuzzlePieces from "../lib/assets/PuzzlePieces.svelte";
 
+</script>
+
+<main class="scroll-container">
+	
+	<h1>Scroll Down!</h1>
+
+	<!-- <PuzzlePieces /> -->
+
+</main>
+
+<style>
+
+	.scroll-container {
+		/* height: 100vh;
+		height: 100dvh;
+		height: 100svh;
+		height: 100lvh; */
+		position: fixed;
+		inset: 0;
+		overflow: hidden scroll;
+		perspective: 10px;
+		background-image: url("$lib/assets/puzzlePieces.webp");
+		background-position: 50% 75%;
+	}
+
+	h1 {
+		position: relative;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		height: 100%;
+		transform-style: preserve-3d;
+		z-index: -1
+	}
+
+</style>
