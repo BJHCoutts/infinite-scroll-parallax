aa5a714036bc88cfafa64f482dbb9e8432b04b61
Brian Coutts <bjhcoutts@gmail.com>
2023-10-20 12:56:37 -0700

    lazy load fade in effect works

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,13 @@
 <script lang='ts'>
 	import { onMount } from "svelte";
 
+	let i = 1
+
+	function handleLoad({currentTarget}: {currentTarget: HTMLElement}) {
+		currentTarget.style.opacity = '100'
+		currentTarget.style.border = '2px solid red'
+	}
+
 	onMount(()=>{
 		const scrollContainer = document.querySelector('.scroll-container')
 		const headers = document.querySelectorAll('h1')
@@ -12,14 +19,25 @@
 
 				if (entry.isIntersecting) {
 	
-					const newEl = document.createElement('h1')
-					newEl.innerText = "Created Scroll Down!"
-					scrollContainer?.appendChild(newEl)
+					const newHeader = document.createElement('h1')
+					newHeader.innerText = "Created Scroll Down!"
+					newHeader.className = 'created-element'
 
-					observer.observe(newEl)
+					const newImage = document.createElement('img')
+					i = i+1
+					newImage.src = `<img src="https://loremflickr.com/500/300?random=${i}" />`
+					newImage.loading = 'lazy'
+
+					scrollContainer?.appendChild(newHeader)
+					scrollContainer?.appendChild(newImage)
+
+
+					observer.observe(newImage)
 					
 				}
 			})
+		}, {
+			threshold: 1
 		})
 
 		if (headers) {
@@ -36,10 +54,13 @@
 
 <main class="scroll-container">
 	
-	<h1>Scroll Down!</h1>
-	<h1>Scroll Down!</h1>
-	<h1>Scroll Down!</h1>
-	<!-- <PuzzlePieces /> -->
+	<h1 class='created-element'>Scroll Down!</h1>
+
+	<div class="grid">
+		<div class="image-container">
+			<img class='created-image' src={`https://loremflickr.com/320/240?random=1`} alt='kitten' on:load={handleLoad}/>
+		</div>
+	</div>
 
 </main>
 
@@ -56,9 +77,28 @@
 		perspective: 10px;
 		background-image: url("$lib/assets/puzzlePieces.webp");
 		background-position: 50% 75%;
+		background-attachment: scroll;
 	}
 
-	h1 {
+	.grid {
+		display: grid;
+		grid-template-columns: 1fr 1fr;
+		grid-template-rows: 1fr;
+		gap: 1em;
+		margin: 0 2em;
+		place-items: center;
+	}
+
+	.image-container {
+		background-color: grey;
+	}
+
+	.created-image {
+		opacity: 0;
+		transition: opacity .3s ease-in;
+	}
+
+	.created-element {
 		position: relative;
 		display: flex;
 		justify-content: center;
@@ -66,6 +106,7 @@
 		height: 100%;
 		transform-style: preserve-3d;
 		z-index: -1;
+		margin-top: 20vh;
 		/* display: none; */
 	}
 
